d8c954e703cda1bae09de1cf4e3679c9172ef867
La-wii <sentilavale@gmail.com>
2021-09-30 18:02:30 +0200

    add tables

---
+++ database/migrations/2021_09_30_154503_create_food_order_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFoodOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('food_order', function (Blueprint $table) {
+            $table->unsignedBigInteger('food_id');
+            $table->foreign('food_id')->references('id')->on('foods');
+
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders');
+
+            $table->integer('food_units');
+            $table->primary(['food_id', 'order_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('food_order');
+    }
+}
