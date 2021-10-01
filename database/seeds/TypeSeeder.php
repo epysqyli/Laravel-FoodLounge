13faaa6a14c71ff39c70b97ecc4177221f5bc52f
La-wii <sentilavale@gmail.com>
2021-10-01 16:01:53 +0200

    type seeder

---
+++ database/seeds/TypeSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Type;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = [
+            'Antipasti',
+            'Primi piatti',
+            'Secondi piatti',
+            'Dessert',
+        ];
+
+        foreach ($types as $type) {
+            $newType = new Type();
+
+            $newType->name = $type;
+
+            $newType->save();
+        }
+    }
+
+}
