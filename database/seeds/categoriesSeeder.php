c8586c8adb10fd96ebd3f0a786696d05210786f8
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-01 11:47:43 +0200

    Add categoriesSeeder

---
+++ database/seeds/categoriesSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class categoriesSeeder extends Seeder
+{
+    public function run()
+    {
+        $categories = [
+            'Cinese',
+            'Giapponese',
+            'Pizzeria',
+            'Fast Food',
+            'Indiano',
+            'Messicano',
+            'Thailandese',
+            'Italiano',
+            'Greco',
+            'Pasticceria'
+        ];
+
+        foreach ($categories as $cat_name) {
+            $category = new Category();
+
+            $category->name = $cat_name;
+            $category->img = "https://picsum.photos/700/500";
+
+            $category->save();
+        }
+    }
+}
