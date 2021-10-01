ccc21f8fffa2979bdb743904190df492d55e165b
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-01 11:28:48 +0200

    Add userSeeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Illuminate\Support\Str;
+use Faker\Factory;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+
+            $user->email = $faker->unique()->email();
+            $user->password = $faker->unique()->password();
+            $user->name = $faker->unique()->company();
+            $user->address = $faker->unique()->address();
+            $user->description = $faker->paragraphs(3, true);
+            $user->vat = rand(11111111111, 99999999999);
+            $user->slug = Str::slug($user->name);
+            $user->profile_image = "https://picsum.photos/300/300";
+            $user->cover_image = "https://picsum.photos/1200/600";
+
+            $user->save();
+        }
+    }
+}
