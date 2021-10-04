28123872a579b9592f3c0fae2ec74aa88ebcbc84
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-10-04 17:40:35 +0200

    Create

--- resources/views/admin/foods/index.blade.php
+++ resources/views/admin/foods/index.blade.php
@@ -2,7 +2,20 @@
 
 @section('content')
     <div class="containerindex">
-       <h1>Foods</h1>
+        <div class="text-center">
+            <h1 class="text-uppercase ">Foods</h1>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <div class="d-grid ">
+                <a href="{{ route('admin.foods.create',Auth::user()->id)}}" > <button class="btn btn-primary block box-shadow" style="width:80%" type="button">Add Food</button></a>
+                   
+                   
+                </div>
+            </div>
+        </div>
+      
+      
        
 
     </div>    
