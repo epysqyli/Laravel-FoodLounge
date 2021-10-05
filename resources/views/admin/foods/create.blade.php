9c38597101891137e64a532b0279b5f901e00da4
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-10-05 15:54:54 +0200

    Changing Details

--- resources/views/admin/foods/create.blade.php
+++ resources/views/admin/foods/create.blade.php
@@ -5,14 +5,14 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Registrati come ristoratore</div>
+                    <div class="card-header text-uppercase ">Add New Plate</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
                             @csrf
 
                             <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">Nome attivit&agrave;</label>
+                                <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
 
                                 <div class="col-md-6">
                                     <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
@@ -27,13 +27,13 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">Indirizzo Email</label>
+                                <label for="ingredients" class="col-md-4 col-form-label text-md-right">Ingredients</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                        name="email" value="{{ old('email') }}" autocomplete="email">
+                                    <input id="ingredients" type="ingredients" class="form-control @error('ingredients') is-invalid @enderror"
+                                        name="ingredients" value="{{ old('ingredients') }}" autocomplete="ingredients">
 
-                                    @error('email')
+                                    @error('ingredients')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
@@ -42,15 +42,15 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="password"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                <label for="price"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Price') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password"
-                                        class="form-control @error('password') is-invalid @enderror" name="password"
-                                        autocomplete="new-password">
+                                    <input id="price" type="price"
+                                        class="form-control @error('price') is-invalid @enderror" name="price"
+                                        autocomplete="new-price">
 
-                                    @error('password')
+                                    @error('price')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
@@ -58,34 +58,12 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Conferma
-                                    Password</label>
+                            
 
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control"
-                                        name="password_confirmation" autocomplete="new-password">
-                                </div>
-                            </div>
+                           
 
                             <div class="form-group row">
-                                <label for="address" class="col-md-4 col-form-label text-md-right">Indirizzo</label>
-
-                                <div class="col-md-6">
-                                    <input id="address" type="text" value="{{ old('address') }}"
-                                        class="form-control @error('address') is-invalid
-                                    @enderror"
-                                        name="address" autocomplete="address">
-                                    @error('address')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="description" class="col-md-4 col-form-label text-md-right">Descrizione</label>
+                                <label for="description" class="col-md-4 col-form-label text-md-right">Description</label>
 
                                 <div class="col-md-6">
                                     <textarea id="description" type="text" value="{{ old('description') }}"
@@ -102,39 +80,18 @@
                                 </div>
                             </div>
 
-                            <!-- <div class="form-group row">
-                                <label for="category" class="col-md-4 col-form-label text-md-right">Categoria
-                                    Ristorante</label>
 
-                                <div class="col-md-6">
-                                    <select id="category"
-                                        class="form-control @error('category') is-invalid
-                                    @enderror"
-                                        name="category" autocomplete="category">
-                                        <option value="">Scegli la tua categoria</option>
-                                        @foreach ($categories as $category)
-                                            <option value={{ $category->id }} @if ($category->id == old('category'))
-                                                selected
-                                        @endif>{{ $category->name }}</option>
-                                        @endforeach
-                                    </select>
-                                    @error('category')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div> -->
-
+                           
                             <div class="form-group row">
-                                <label for="vat" class="col-md-4 col-form-label text-md-right">Partita IVA</label>
+                                <label for="image" class="col-md-4 col-form-label text-md-right">Image
+                                    Food</label>
 
                                 <div class="col-md-6">
-                                    <input id="vat" type="text" value="{{ old('vat') }}"
-                                        class="form-control @error('vat') is-invalid
+                                    <input id="image" type="file"
+                                        class="form-control @error('image') is-invalid
                                     @enderror"
-                                        name="vat" autocomplete="vat">
-                                    @error('vat')
+                                        name="image" autocomplete="image">
+                                    @error('image')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
@@ -142,39 +99,7 @@
                                 </div>
                             </div>
 
-                            <div class="form-group row">
-                                <label for="profile_image" class="col-md-4 col-form-label text-md-right">Immagine
-                                    Profilo</label>
-
-                                <div class="col-md-6">
-                                    <input id="profile_image" type="file"
-                                        class="form-control @error('profile_image') is-invalid
-                                    @enderror"
-                                        name="profile_image" autocomplete="profile_image">
-                                    @error('profile_image')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="cover_image" class="col-md-4 col-form-label text-md-right">Immagine di
-                                    Copertina</label>
-
-                                <div class="col-md-6">
-                                    <input id="cover_image" type="file"
-                                        class="form-control @error('cover_image') is-invalid
-                                    @enderror"
-                                        name="cover_image" autocomplete="cover_image">
-                                    @error('cover_image')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
+                            
 
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
