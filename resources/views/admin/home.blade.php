037cfc26d9edd47a27587f18a149ce39f7da2a9e
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-10-05 19:23:01 +0200

    edochange background

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,22 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="cardrestaurant d-flex justify-content-center">
-        <div class="card">
-            <img src="{{ URL::asset('/image/backend.jpg') }}" alt="John" style="width:100%">
-            <h1>Nome Ristorante</h1>
-            <p class="title">{{ Auth::user()->name }}</p>
-
-            <div style="margin: 24px 0;">
-                <a class="nav-link text-uppercase" href="{{ route('admin.foods.index', Auth::user()->id) }}">Foods</a>
-                <p class="card-text"><a class="nav-link text-uppercase"
-                        href="{{ route('admin.orders.index', Auth::user()->id) }}">Orders</a></p>
-                <a href="#"><i class="fa fa-linkedin"></i></a>
-                <a href="#"><i class="fa fa-facebook"></i></a>
+    <div class="cardrestaurant container">
+        <div class="row">
+            <div class="col-3 align-item-end">
+                menu
             </div>
-            <p><button>Contact</button></p>
+            <div class="col-6">
+                <div class="card">
+                    <img src="{{ URL::asset('/image/backend.jpg') }}" alt="John" style="width:100%">
+                    <h1>Nome Ristorante</h1>
+                    <p class="title">{{ Auth::user()->name }}</p>
+
+                    <div style="margin: 24px 0;">
+                        <a class="nav-link text-uppercase" href="{{ route('admin.foods.index', Auth::user()->id) }}">Foods</a>
+                        <p class="card-text"><a class="nav-link text-uppercase"
+                                href="{{ route('admin.orders.index', Auth::user()->id) }}">Orders</a></p>
+                        <a href="#"><i class="fa fa-linkedin"></i></a>
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                    </div>
+                    <p><button>Contact</button></p>
+                </div>
+             </div>
+             <div class="col-3">
+                 menu
+             </div>
         </div>
     </div>
 
+   
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,7 +72,7 @@
             </div>
         </nav>
 
-        <main class="py-4 bg-light">
+        <main class="py-4">
             @yield('content')
         </main>
     </div>
