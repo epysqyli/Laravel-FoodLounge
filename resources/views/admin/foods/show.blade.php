6e12b1355cc743755e0b42da872c952be91738aa
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-05 22:24:16 +0200

    Add food show

--- resources/views/admin/foods/show.blade.php
+++ resources/views/admin/foods/show.blade.php
@@ -1,5 +1,47 @@
 @extends('layouts.app')
 
-@section('content')
+{{-- @dd($food); --}}
 
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="mt-2 mb-5">{{ $food->name }}</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <img class="rounded" src={{ $food->image }} alt="">
+            </div>
+            <div class="col-8">
+                <table class="table table-light table-striped">
+                    <tr>
+                        <th>Price
+                        <th>
+                        <td>{{ $food->id }}</td>
+                    </tr>
+                    <tr>
+                        <th>Type ID
+                        <th>
+                        <td>{{ $food->type_id }}</td>
+                    </tr>
+                    <tr>
+                        <th>Price
+                        <th>
+                        <td>{{ $food->price }} &euro;</td>
+                    </tr>
+                    <tr>
+                        <th>Description
+                        <th>
+                        <td>{{ $food->description }}</td>
+                    </tr>
+                    <tr>
+                        <th>Visible
+                        <th>
+                        <td>{{ $food->visible ? 'Yes' : 'No' }}</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
