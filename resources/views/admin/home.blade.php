c82bd6c2cf1a0954dcd6ae8ad2d0b641de8329ac
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-05 21:55:06 +0200

    Add more bootstrap styles to home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <div class="row mx-1 pt-4 rounded shadow">
+        <div class="row mx-1 pt-4 rounded border bg-light">
             <div class="col-4">
                 <div class="row">
                     <div class="col-8 offset-2">
@@ -10,19 +10,22 @@
                     </div>
                 </div>
                 <div class="row">
-                    <a class="col-8 mx-auto my-2 pt-2 pb-1 border shadow" href="{{ route('admin.foods.index') }}">
+                    <a class="col-7 mx-auto my-2 pt-2 pb-1 border rounded shadow bg-info text-white" href="{{ route('admin.foods.index') }}">
                         <h4 class="text-center">Manage Foods</h4>
                     </a>
                 </div>
                 <div class="row">
-                    <a class="col-8 mx-auto mt-3 pt-2 pb-1 border shadow" href="{{ route('admin.orders.index') }}">
+                    <a class="col-7 mx-auto mt-3 pt-2 pb-1 border rounded shadow bg-info text-white" href="{{ route('admin.orders.index') }}">
                         <h4 class="text-center">Check Orders</h4>
                     </a>
                 </div>
             </div>
             <div class="col-8">
-                <img src={{ asset('storage/' . Auth::user()->cover_image) }} alt="John" style="width:100%">
+                <img class="rounded shadow" src={{ asset('storage/' . Auth::user()->cover_image) }} alt="John" style="width:100%">
                 <h1 class="title my-3">{{ Auth::user()->name }}</h1>
+                <p class="lead">
+                    {{ Auth::user()->description }}
+                </p>
             </div>
         </div>
     </div>
