acc8f70edfbcba621758eed1e98ac147237b48dd
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-05 18:37:33 +0200

    Complete admin home layout

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="row mx-1 border border-dark rounded shadow">
+            <div class="col py-5">
+                <h2 class="text-center">Logged restaurant details</h2>
+            </div>
+        </div>
+    </div>
+
     <div class="container py-4">
         <div class="row">
             <a class="col my-2 my-sm-0 mx-3 border border-dark rounded py-5 shadow-sm" href="{{ route('admin.foods.index') }}">
