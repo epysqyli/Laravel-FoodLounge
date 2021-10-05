102c1b167cf1632e470351107bd33a338e6abefe
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-05 19:17:42 +0200

    Remove image from index foods

--- resources/views/admin/foods/index.blade.php
+++ resources/views/admin/foods/index.blade.php
@@ -1,21 +1,18 @@
 @extends('layouts.app')
 
-{{-- @dd($foods); --}}
-
 @section('content')
-    <div class="container w-100">
+    <div class="container">
         <h1>All foods</h1>
         <table class="table table-light table-striped">
             <thead>
                 <tr>
-                    <th scope="col">Food ID</th>
-                    <th scope="col">Image</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Visible</th>
-                    <th scope="col">Details</th>
-                    <th scope="col">Edit</th>
-                    <th scope="col">Delete</th>
+                    <th>Food ID</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Visible</th>
+                    <th>Details</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
                 </tr>
             </thead>
 
@@ -23,14 +20,12 @@
                 @foreach ($foods as $food)
                     <tr>
                         <td>{{ $food->id }}</td>
-                        <td><img class="w-50" src="{{ $food->image }}" alt="food-image"></td>
                         <td>{{ $food->name }}</td>
                         <td>{{ $food->price }} &euro;</td>
                         <td>{{ $food->visible ? 'Visible' : 'Not Visible' }}</td>
-                        <td>Show</td>
+                        <td><a href="{{ route('admin.foods.show', $food->id) }}">Check Details</a></td>
                         <td>Edit</td>
                         <td>Delete</td>
-                        <td><a href="{{ route('admin.foods.show', $food->id) }}">Check Details</a></td>
                     </tr>
                 @endforeach
             </tbody>
