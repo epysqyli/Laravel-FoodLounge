80d28b2afd3068f9ef5ef46fb0217452fa256a42
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-05 20:24:00 +0200

    Remove foods.index th

--- resources/views/admin/foods/index.blade.php
+++ resources/views/admin/foods/index.blade.php
@@ -2,33 +2,33 @@
 
 @section('content')
     <div class="container">
-        <h1>All foods</h1>
-        <table class="table table-light table-striped">
-            <thead>
-                <tr>
-                    <th>Food ID</th>
-                    <th>Name</th>
-                    <th>Price</th>
-                    <th>Visible</th>
-                    <th>Details</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
-                </tr>
-            </thead>
+        <div class="row">
+            <div class="col">
+                <h1>All foods</h1>
+                <table class="table table-light table-striped">
+                    <thead>
+                        <tr>
+                            <th>Food ID</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Visible</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
 
-            <tbody>
-                @foreach ($foods as $food)
-                    <tr>
-                        <td>{{ $food->id }}</td>
-                        <td>{{ $food->name }}</td>
-                        <td>{{ $food->price }} &euro;</td>
-                        <td>{{ $food->visible ? 'Visible' : 'Not Visible' }}</td>
-                        <td><a href="{{ route('admin.foods.show', $food->id) }}">Check Details</a></td>
-                        <td>Edit</td>
-                        <td>Delete</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+                    <tbody>
+                        @foreach ($foods as $food)
+                            <tr>
+                                <td>{{ $food->id }}</td>
+                                <td>{{ $food->name }}</td>
+                                <td>{{ $food->price }} &euro;</td>
+                                <td>{{ $food->visible ? 'Visible' : 'Not Visible' }}</td>
+                                <td><a href="{{ route('admin.foods.show', $food->id) }}">Check Details</a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 @endsection
