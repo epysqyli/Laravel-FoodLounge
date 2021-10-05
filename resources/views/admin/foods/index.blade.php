df6b3bffad9e1ad891b6d2732b4d1e513d427005
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-05 18:53:49 +0200

    Add foods index

--- resources/views/admin/foods/index.blade.php
+++ resources/views/admin/foods/index.blade.php
@@ -1,5 +1,39 @@
 @extends('layouts.app')
 
-@section('content')
+{{-- @dd($foods); --}}
 
+@section('content')
+    <div class="container w-100">
+        <h1>All foods</h1>
+        <table class="table table-light table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Food ID</th>
+                    <th scope="col">Image</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Visible</th>
+                    <th scope="col">Details</th>
+                    <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($foods as $food)
+                    <tr>
+                        <td>{{ $food->id }}</td>
+                        <td><img class="w-50" src="{{ $food->image }}" alt="food-image"></td>
+                        <td>{{ $food->name }}</td>
+                        <td>{{ $food->price }} &euro;</td>
+                        <td>{{ $food->visible ? 'Visible' : 'Not Visible' }}</td>
+                        <td>Show</td>
+                        <td>Edit</td>
+                        <td>Delete</td>
+                        <td><a href="{{ route('admin.foods.show', $food->id) }}">Check Details</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
