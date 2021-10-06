fda3e53fb5a4d5b400b9ea0218c799aec49697fd
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-06 14:39:19 +0200

    Revert navbar change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,10 +52,7 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item">
-                                <a class="navbar-brand border rounded px-1 bg-light" href="{{ route('logout') }}">Logout</a>
-                            </li>
-                            {{-- <li class="nav-item dropdown">
+                            <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
@@ -63,7 +60,7 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                             document.getElementById('logout-form').submit();">
+                                                                 document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -72,7 +69,7 @@
                                         @csrf
                                     </form>
                                 </div>
-                            </li> --}}
+                            </li>
                         @endguest
                     </ul>
                 </div>
