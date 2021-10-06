78a64c48011e7e5eb9428a7e2dd27b91e12d317f
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-06 09:25:29 +0200

    Add food types dropdown menu to foods create

--- resources/views/admin/foods/create.blade.php
+++ resources/views/admin/foods/create.blade.php
@@ -58,9 +58,28 @@
                                 </div>
                             </div>
 
-                            
+                            <div class="form-group row">
+                                <label for="type" class="col-md-4 col-form-label text-md-right">Categoria Piatto</label>
 
-                           
+                                <div class="col-md-6">
+                                    <select id="type"
+                                        class="form-control @error('type') is-invalid
+                                    @enderror"
+                                        name="type" autocomplete="type">
+                                        <option value="">Scegli la tua categoria</option>
+                                        @foreach ($types as $type)
+                                            <option value={{ $type->id }} @if ($type->id == old('type'))
+                                                selected
+                                        @endif>{{ $type->name }}</option>
+                                        @endforeach
+                                    </select>
+                                    @error('type')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
 
                             <div class="form-group row">
                                 <label for="description" class="col-md-4 col-form-label text-md-right">Description</label>
@@ -80,8 +99,6 @@
                                 </div>
                             </div>
 
-
-                           
                             <div class="form-group row">
                                 <label for="image" class="col-md-4 col-form-label text-md-right">Image
                                     Food</label>
