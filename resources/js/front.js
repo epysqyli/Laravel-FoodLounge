ac3e0fa189f3963a10c2f4664a19613e9874cc8f
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-07 17:33:31 +0200

    Remove unnecessary comments from js files

--- resources/js/front.js
+++ resources/js/front.js
@@ -4,14 +4,8 @@ window.Vue = require("vue");
 window.axios = require("axios");
 
 //import Vue from 'vue';
-import App from './views/App';
+import App from "./views/App";
 import router from "./router";
-
-
-// Vue.component(
-//     "example-component",
-//     require("./components/ExampleComponent.vue").default
-// );
 
 const app = new Vue({
     el: "#root",
--- resources/js/router.js
+++ resources/js/router.js
@@ -1,25 +1,25 @@
-import Vue from 'vue';  
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 
 // Importation page of Vue.Router
 import Home from "./pages/Home";
 import Category from "./pages/Category";
-Vue.use( VueRouter );
+Vue.use(VueRouter);
 
 const router = new VueRouter({
-    mode: 'history',
+    mode: "history",
     routes: [
         {
-            path: '/',
-            name: 'Home',
+            path: "/",
+            name: "Home",
             component: Home
         },
         {
-            path: '/category/:id',
-            name: 'category',
+            path: "/category/:id",
+            name: "category",
             component: Category
-        },
-    ],
+        }
+    ]
 });
 
 export default router;
