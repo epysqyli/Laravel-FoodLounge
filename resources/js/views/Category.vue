d5f391626e90d3d783f0c7a6707c092e1153937a
La-wii <sentilavale@gmail.com>
2021-10-07 18:18:26 +0200

    cose da cambiare

---
+++ resources/js/views/Category.vue
@@ -0,0 +1,53 @@
+<template>
+    <main>
+        <div class="container">
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <p>Categoria</p>
+                </div>
+            </div>
+
+        
+            <div class="row">
+                <div class="col-xs-12 mx-auto">
+                    <div class="card mb-3" style="max-width: 800px;">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <!-- <img src="..." class="img-fluid rounded-start" alt="..."> -->
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <!-- <div class="row">
+                <div class="col-6" v-for="post in posts" :key="post.id">
+                    <div class="card mb-3">                   
+                        <div class="card-body">
+                            <h5 class="card-title">{{post.title}}</h5>
+                            <p class="card-text">{{post.content}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div> -->
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "Category",
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/js/views/Restaurant.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12 text-center p-5">
+                <h2 class="mx-auto">Nome ristorante</h2>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 text-center p-5">
+                <h4 class="mx-auto">Menù</h4>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+    name: "Restaurant",
+}
+</script>
+
+<style lang="scss" scoped>
+h2{
+    font-size: 40px;	
+    color: #ffd60a;
+}
+
+h4{
+    font-size: 25px;
+    color: #ffd60a;
+}
+</style>    
