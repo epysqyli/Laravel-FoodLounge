db37ccd238514110bb52f0018369ab552655a0b8
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-10-07 14:00:35 +0200

    Editing Description

--- resources/views/admin/foods/edit.blade.php
+++ resources/views/admin/foods/edit.blade.php
@@ -20,9 +20,10 @@
                             </div>
                         @endif --}}
 
-                        <form method="post" action="{{ route('admin.foods.store', Auth::user()->id) }}"
+                        <form method="post" action="{{ route('admin.foods.update', Auth::user()->id) }}"
                             enctype="multipart/form-data">
                             @csrf
+                            @method('PATCH')
 
                             <input type="hidden" id='user_id' name='user_id' value={{ Auth::user()->id }}>
 
@@ -31,7 +32,7 @@
 
                                 <div class="col-md-6">
                                     <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
-                                        name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
+                                        name="name" value="{{ old('name',$food->name) }}" autocomplete="name" autofocus>
 
                                     @error('name')
                                         <span class="invalid-feedback" role="alert">
@@ -45,11 +46,11 @@
                                 <label class="col-md-3 offset-md-1 text-md-right pt-md-1" for="visible">Availability</label>
                                 <div class="col-md-4 col-form-label">
                                     <div>
-                                        <input type="radio" id="visible" name="visible" value="1">
+                                        <input type="radio" id="visible" name="visible" value="{{$food->true}}" >
                                         <label for="visible">Accessible</label><br>
                                     </div>
                                     <div>
-                                        <input type="radio" id="nonvisible" name="visible" value="0">
+                                        <input type="radio" id="nonvisible" name="visible" value="{{$food->false}}" >
                                         <label for="nonvisible">Inaccessible</label><br>
                                     </div>
 
@@ -67,7 +68,7 @@
                                 <div class="col-md-6">
                                     <input id="price" type="number" step="0.01"
                                         class="form-control @error('price') is-invalid @enderror" name="price"
-                                        autocomplete="price" value="{{ old('price') }}">
+                                        autocomplete="price" value="{{ old('price',$food->price) }}">
 
                                     @error('price')
                                         <span class="invalid-feedback" role="alert">
@@ -83,7 +84,7 @@
                                 <div class="col-md-6">
                                     <input id="ingredients" type="text"
                                         class="form-control @error('ingredients') is-invalid @enderror" name="ingredients"
-                                        value="{{ old('ingredients') }}" autocomplete="ingredients">
+                                        value="{{ old('ingredients',$food->ingredients) }}" autocomplete="ingredients">
 
                                     @error('ingredients')
                                         <span class="invalid-feedback" role="alert">
@@ -120,8 +121,8 @@
                                 <label for="description" class="col-md-4 col-form-label text-md-right">Description</label>
 
                                 <div class="col-md-6">
-                                    <textarea id="description" type="text" value="{{ old('description') }}"
-                                        class="form-control @error('description') is-invalid
+                                    <textarea id="description" type="text" value="{{ old('description',$food->description) }}"
+                                        class="form-control text-left @error('description') is-invalid
                                     @enderror"
                                         name="description" autocomplete="description" rows="6">{{ old('description') }}
                                                                                     </textarea>
@@ -141,7 +142,7 @@
                                     <input id="image" type="file"
                                         class="d-block form-control-file @error('image') is-invalid
                                     @enderror"
-                                        name="image" autocomplete="image">
+                                        name="image" autocomplete="image" value="{{old('image',$food->image)}}">
                                     @error('image')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -155,7 +156,7 @@
                                     Details</label>
 
                                 <div class="col-md-6">
-                                    <textarea id="additional_details" type="text" value="{{ old('additional_details') }}"
+                                    <textarea id="additional_details" type="text" value="{{ old('additional_details',$food->additional_details) }}"
                                         class="form-control" name="additional_details" autocomplete="additional_details"
                                         rows="3">{{ old('additional_details') }}
                                             </textarea>
