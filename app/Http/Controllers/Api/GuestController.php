3b581bb06b682d2d1c7463f8d0fed9869ea77844
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-08 21:21:03 +0200

    Add query to restaurant api to fetch only visible plates

--- app/Http/Controllers/Api/GuestController.php
+++ app/Http/Controllers/Api/GuestController.php
@@ -9,19 +9,24 @@ use App\User;
 
 class GuestController extends Controller
 {
-    public function categories() {
+    public function categories()
+    {
         $categories = Category::all();
         return response()->json(compact('categories'));
     }
 
-    public function category($slug) {
+    public function category($slug)
+    {
         $category = Category::where('slug', $slug)->first();
         $data = $category->users;
         return response()->json($data);
     }
 
-    public function restaurant($slug) {
-        $data = User::where('slug', $slug)->with('foods')->first();
+    public function restaurant($slug)
+    {
+        $data = User::where('slug', $slug)->with(['foods' => function ($query) {
+            $query->where('visible', '1');
+        }])->first();
         return response()->json($data);
     }
 }
