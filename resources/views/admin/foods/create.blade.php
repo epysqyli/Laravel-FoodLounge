422acd41ec31af2e4b07d222f0d584794325aa19
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-08 09:53:04 +0200

    Update create foods

--- resources/views/admin/foods/create.blade.php
+++ resources/views/admin/foods/create.blade.php
@@ -17,6 +17,13 @@
 
                             <input type="hidden" id='user_id' name='user_id' value={{ Auth::user()->id }}>
 
+                            @if (count(Auth::user()->types) == 0)
+                                <div class="form-group row">
+                                    <a class="col-md-6 offset-md-4 d-block border border-danger btn btn-outline-danger "
+                                        href={{ route('admin.types.create') }}>Add food types before creating foods</a>
+                                </div>
+                            @endif
+
                             <div class="form-group row">
                                 <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
 
@@ -56,7 +63,8 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="price" class="col-md-4 col-form-label text-md-right">{{ __('Price') }}</label>
+                                <label for="price"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Price') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="price" type="number" step="0.01"
@@ -89,96 +97,91 @@
                                 </div>
                             </div>
 
-                            @if (count(Auth::user()->types) != 0)
-                                <div class="form-group row">
-                                    <label for="type_id" class="col-md-4 col-form-label text-md-right">Type</label>
+                            <div class="form-group row">
+                                <label for="type_id" class="col-md-4 col-form-label text-md-right">Type</label>
 
-                                    <div class="col-md-6">
-                                        <select id="type"
-                                            class="form-control @error('type') is-invalid
+                                <div class="col-md-6">
+                                    <select id="type"
+                                        class="form-control @error('type') is-invalid
                                     @enderror"
-                                            name="type_id" autocomplete="type_id">
-                                            <option value="">Choose your types</option>
-                                            @foreach ($types as $type)
-                                                <option value={{ $type->id }} @if ($type->id == old('type'))
-                                                    selected
-                                            @endif>{{ $type->name }}</option>
-                            @endforeach
-                            </select>
-                            @error('type')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                    </div>
-                </div>
-            @else
-                <div class="form-group row">
-                    <a class="col-md-6 offset-md-4 d-block border border-danger btn btn-outline-danger "
-                        href={{ route('admin.types.create') }}>Add food types before creating foods</a>
-                </div>
-                @endif
+                                        name="type_id" autocomplete="type_id"
+                                        {{ count(Auth::user()->types) == 0 ? 'disabled' : '' }}>
+                                        <option value="">Choose your types</option>
+                                        @foreach ($types as $type)
+                                            <option value={{ $type->id }} @if ($type->id == old('type'))
+                                                selected
+                                        @endif>{{ $type->name }}</option>
+                                        @endforeach
+                                    </select>
+                                    @error('type')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
 
-                <div class="form-group row">
-                    <label for="description" class="col-md-4 col-form-label text-md-right">Description</label>
+                            <div class="form-group row">
+                                <label for="description" class="col-md-4 col-form-label text-md-right">Description</label>
 
-                    <div class="col-md-6">
-                        <textarea id="description" type="text" value="{{ old('description') }}"
-                            class="form-control @error('description') is-invalid
+                                <div class="col-md-6">
+                                    <textarea id="description" type="text" value="{{ old('description') }}"
+                                        class="form-control @error('description') is-invalid
                                     @enderror"
-                            name="description" autocomplete="description" rows="6"
-                            {{ count(Auth::user()->types) == 0 ? 'disabled' : '' }}>{{ old('description') }}
-                                                                                            </textarea>
+                                        name="description" autocomplete="description" rows="6"
+                                        {{ count(Auth::user()->types) == 0 ? 'disabled' : '' }}>{{ old('description') }}
+                                                                                                    </textarea>
 
-                        @error('description')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                </div>
+                                    @error('description')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
 
-                <div class="form-group row">
-                    <label for="image" class="col-md-4 col-form-label text-md-right">Image Food</label>
+                            <div class="form-group row">
+                                <label for="image" class="col-md-4 col-form-label text-md-right">Image Food</label>
 
-                    <div class="col-md-6">
-                        <input id="image" type="file"
-                            class="d-block form-control-file @error('image') is-invalid
+                                <div class="col-md-6">
+                                    <input id="image" type="file"
+                                        class="d-block form-control-file @error('image') is-invalid
                                     @enderror"
-                            name="image" autocomplete="image" {{ count(Auth::user()->types) == 0 ? 'disabled' : '' }}>
-                        @error('image')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
+                                        name="image" autocomplete="image"
+                                        {{ count(Auth::user()->types) == 0 ? 'disabled' : '' }}>
+                                    @error('image')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="additional_details" class="col-md-4 col-form-label text-md-right">Additional
+                                    Details</label>
+
+                                <div class="col-md-6">
+                                    <textarea id="additional_details" type="text" value="{{ old('additional_details') }}"
+                                        class="form-control" name="additional_details" autocomplete="additional_details"
+                                        rows="3" {{ count(Auth::user()->types) == 0 ? 'disabled' : '' }}>{{ old('additional_details') }}
+                                                            </textarea>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary"
+                                        {{ count(Auth::user()->types) == 0 ? 'disabled' : '' }}>
+                                        Add plates
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
-
-                <div class="form-group row">
-                    <label for="additional_details" class="col-md-4 col-form-label text-md-right">Additional
-                        Details</label>
-
-                    <div class="col-md-6">
-                        <textarea id="additional_details" type="text" value="{{ old('additional_details') }}"
-                            class="form-control" name="additional_details" autocomplete="additional_details" rows="3"
-                            {{ count(Auth::user()->types) == 0 ? 'disabled' : '' }}>{{ old('additional_details') }}
-                                                    </textarea>
-                    </div>
-                </div>
-
-                <div class="form-group row mb-0">
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="btn btn-primary"
-                            {{ count(Auth::user()->types) == 0 ? 'disabled' : '' }}>
-                            Add plates
-                        </button>
-                    </div>
-                </div>
-                </form>
             </div>
         </div>
-    </div>
-    </div>
     </div>
 
 @endsection
