ea77524e2ab9ea1eea814e0680be49f8bf6938fc
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-10-08 14:14:45 +0200

    Category cards

--- resources/js/views/Category.vue
+++ resources/js/views/Category.vue
@@ -1,58 +1,38 @@
 <template>
   <main>
     <div class="container">
-      <div class="row">
-        <div class="col-12 text-center p-5">
-          <h2 class="text-center mx-auto"></h2>
-        </div>
-      </div>
-
-      <!-- start restaurants cards to loop -->
-      <div class="row">
-        <div class="col-xs-12 mx-auto">
-          <div class="card mb-3" style="max-width: 800px">
-            <div class="row g-0">
-              <div class="col-md-4">
-                <!-- <img src="..." class="img-fluid rounded-start" alt="..."> -->
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">{{ category.name }}</h5>
-                  <p class="card-text">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </p>
-                  <p class="card-text">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                  </p>
-                </div>
-
-
-                   <div class="col-sm-6" v-for="restaurant in restaurants" :key="restaurant.id">
-
-                              <div class="card mt-3">
-
-                                <div class="card-body">
-
-                                  <h5 class="card-title">{{restaurant.name}}</h5>
-
-              <!-- <p>{{restaurant.created_at}}</p>
-              <p class="card-text">{{resta.content}}</p>
-              <router-link :to="{ name: 'post-detail', params:{slug: post.slug}}" class="btn btn-primary">Details</router-link>
-            </div> -->
-          </div>
-          
-        </div>
-              </div>
+      <div class="row row-cols-1 row-cols-md-2 g-4 ">
+           <div class="col" v-for="restaurant in restaurants" :key="restaurant.id">
+               <div class="card">
+                  <img src="" class="card-img-top" alt="...">
+                       <div class="card-body">
+                          <h5 class="card-title">{{restaurant.name}}</h5>
+                          <p>{{restaurant.address}}</p>
+                             <p class="card-text">{{restaurant.description}}</p>
+                        </div>
+                 </div>
+             </div>
+ 
+      </div>  
+       <!-- <div class="row">
+        <div class="col-sm-6" v-for="restaurant in restaurants" :key="restaurant.id">
+          <div class="card mt-3">
+            <div class="card-body">
+              <h5 class="card-title">{{restaurant.name}}</h5>
+              <p>{{restaurant.address}}</p>
+              <p class="card-text">{{restaurant.description}}</p>
+               <router-link :to="{ name: 'post-detail', params:{slug: post.slug}}" class="btn btn-primary">Details</router-link> 
             </div>
           </div>
-        </div>
-      </div>
-      <!-- end restaurants cards to loop -->
+          
+        </div>  -->
     </div>
+    
+
+      
   </main>
 </template>
+
 
 <script>
 export default {
@@ -68,7 +48,7 @@ export default {
          },
         methods:{
           getRestaurant() {
-            
+
             axios.get(this.apiUrl + this.$route.params.slug)  
 
                 .then( response => {
