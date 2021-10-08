4febcca0539462eb3e99fee8b15553d657d4ce5c
La-wii <sentilavale@gmail.com>
2021-10-08 17:27:53 +0200

    category card

--- resources/js/views/Category.vue
+++ resources/js/views/Category.vue
@@ -1,56 +1,36 @@
 <template>
   <main>
-    <div class="container">
+    <div class="container-fluid">
 
       <div class="row">
-        <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3 g-2" v-for="restaurant in restaurants" :key="restaurant.id">
-          <div class="car">
-          
-
-            <div class="flip-box">
-            
-              <div class="flip-box-inner">
-                <div class="flip-box-front">
-                  <img v-bind:src="restaurant.profile_image" class="car-img-top" alt="...">
-                    <div class="text-block">
-                      <h4>Nature</h4>
-                      <p>What a beautiful sunrise</p>
-                    </div>
-                </div>
-                <div class="flip-box-back">
-                  <p>{{restaurant.address}}</p>
-                            
-                 
-                  
-                </div>
-              </div>
-            
-              <!-- <div class="card-body">
-                <h5 class="card-title">{{restaurant.name}}</h5>
-                <p>{{restaurant.address}}</p>
-                <p class="card-text">{{restaurant.description}}</p>
-              </div> -->
-            </div>
-          </div>
-        </div> 
-      </div>  
-
-
-       <!-- <div class="row">
-        <div class="col-sm-6" v-for="restaurant in restaurants" :key="restaurant.id">
-          <div class="card mt-3">
+        <div class="col-xs-12 mx-auto" v-for="restaurant in restaurants" :key="restaurant.id">
+          <!-- <div class="card">
+            <img v-bind:src="restaurant.profile_image" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">{{restaurant.name}}</h5>
               <p>{{restaurant.address}}</p>
               <p class="card-text">{{restaurant.description}}</p>
-               <router-link :to="{ name: 'post-detail', params:{slug: post.slug}}" class="btn btn-primary">Details</router-link> 
+            </div>
+          </div> -->
+          <div class="card m-5" style="max-width: 900px;">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img v-bind:src="restaurant.profile_image" class="card-img-top" alt="restaurant.name">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">{{restaurant.name}}</h5>
+                  <p>{{restaurant.address}}</p>
+                  <p class="card-text">{{restaurant.description}}</p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>  -->
-    </div>
-    
+        </div>
+      </div>
 
-      
+    </div>
+
   </main>
 </template>
 
@@ -69,9 +49,7 @@ export default {
          },
         methods:{
           getRestaurant() {
-
             axios.get(this.apiUrl + this.$route.params.slug)  
-
                 .then( response => {
                   console.log(response);
                      this.restaurants = response.data;
@@ -84,44 +62,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
 h2 {
   font-size: 40px;
   color: #ffd60a;
 }
-.text-block {
-  position: absolute;
-  bottom: 20px;
-  right: 20px;
-  background-color: black;
-  color: white;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-.car {
-  height:300px;
-  max-width:200px;
-  border: none;
-  position:relative;
- 
-  img{
-    height: 100%;
-    width: 100%;
-    
-  }
-}
 
-.car:hover {
+.card {
+  border: none;
+}
+.card:hover {
   box-shadow: 0 0 20px #ffd60a;
 }
-
  /* The flip box container - set the width and height to whatever you want. We have added the border property to demonstrate that the flip itself goes out of the box on hover (remove perspective if you don't want the 3D effect */
 .flip-box {
-  width:200px;
-  height: 300px;
   background-color:white;    
   perspective: 1000px; /* Remove this if you don't want the 3D effect */
 }
-
 /* This container is needed to position the front and back side */
 .flip-box-inner {
   position: relative;
@@ -131,12 +88,10 @@ h2 {
   transition: transform 0.5s;
   transform-style: preserve-3d;
 }
-
 /* Do an horizontal flip when you move the mouse over the flip box container */
 .flip-box:hover .flip-box-inner {
   transform: rotateY(180deg);
 }
-
 /* Position the front and back side */
 .flip-box-front, .flip-box-back {
   position: absolute;
@@ -144,16 +99,14 @@ h2 {
   height: 100%;  
   backface-visibility:hidden;
 }
-
 /* Style the front side (fallback if image is missing) */
 .flip-box-front {
-  background-color: red;
+  background-color: #bbb;
   color: black;
 }
-
 /* Style the back side */
 .flip-box-back {
-  background-color:grey ;
+  background-color: rgb(73, 130, 187);
   color: rgb(5, 5, 5);
   transform: rotateY(180deg);
 } 
