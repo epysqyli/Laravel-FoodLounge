5708a1a842e5e237c3ac3cf2b088d8eda3527c77
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-10 11:39:54 +0200

    Restore categories.vue

--- resources/js/views/Categories.vue
+++ resources/js/views/Categories.vue
@@ -1,69 +1,32 @@
 <<template>
-  
-   <main>
-        <div class="container ">
-
-            <div class="row">
-              <div class="carousel">
-             
-                 <div class="carouselslide">
-                  
-                    <hooper :itemsToShow="3" style="height: 900px">
-                               <slide class="col-3 mx-auto" v-for="restaurant in restaurants" :key="restaurant.id">
-                              <router-link :to="{ name: 'restaurants', params: { slug: restaurant.slug } }" class="car m-5"
-                                style="max-width: 900px;">
-                                <div class="row g-0">
-                                    <div class="col-md-4">
-                                      <div class="car">
-                          
-                                          <div class="flip-box">
-
-                                              <div class="flip-box-inner">
-                                                        <div class="flip-box-front">
-                                                            <img :src="restaurant.profile_image[0] == 'h' ? restaurant.profile_image : `http://localhost:8000/storage/${restaurant.profile_image}`"
-                                                                class="card-img-top" alt="restaurant.name">
-                                                              
-                                                                <div class="text-block">
-                                                                    <h4>{{restaurant.name}}</h4>
-                                                                    <p>What a beautiful sunrise</p>
-                                                                </div>
-                                                        </div>
-                                                    
-                                                        
-                                                        <div class="flip-box-back">
-                                                            <p>{{restaurant.address}}</p>
-
-                                                        </div>
-                                              </div>
-                                        </div>
-                                      </div>
-                                    </div>
-                                </div>
-                              </router-link>
-                      </slide>
-                      
-                      </hooper>
-                    </div>
-    
-                
-               </div>
+  <main>
+    <div class="container mx-auto">
+      <div class="row">
+        <div class="col-12 mx-auto" v-for="restaurant in restaurants" :key="restaurant.id">
+          <router-link :to="{ name: 'restaurants', params: { slug: restaurant.slug } }"
+          class="card m-5" style="max-width: 900px;">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img :src="restaurant.profile_image[0] == 'h' ? restaurant.profile_image : `http://localhost:8000/storage/${restaurant.profile_image}`" class="card-img-top" alt="restaurant.name">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">{{restaurant.name}}</h5>
+                  <p>{{restaurant.address}}</p>
+                  <p class="card-text">{{restaurant.description}}</p>
+                </div>
+              </div>
             </div>
-         </div>
-      
-
-      
+          </router-link>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
-import { Hooper, Slide } from 'hooper';
-import 'hooper/dist/hooper.css';
 export default {
   name: "Category",
-  components:{
-    Hooper,
-    Slide
-  },
   data() {
     return {
       apiUrl: "http://127.0.0.1:8000/api/categories/",
@@ -93,110 +56,18 @@ export default {
   },
 };
 </script>
+
 <style lang="scss" scoped>
 h2 {
   font-size: 40px;
   color: #ffd60a;
 }
-.text-block {
-  position: absolute;
-  bottom: 20px;
-  width: 100%;
-  background: rgb(0, 0, 0); /* Fallback color */
-  background: rgba(0, 0, 0, 0.5);
-  color: white;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-.car {
-  height:300px;
-  width:200px;
+
+.card {
   border: none;
-  position:relative;
-  border-radius: 20px;
- 
-  img{
-    height: 100%;
-    width: 100%;
-    border-radius: 20px;
-    
-  }
 }
 
-.car:hover {
+.card:hover {
   box-shadow: 0 0 20px #ffd60a;
 }
-
- /* The flip box container - set the width and height to whatever you want. We have added the border property to demonstrate that the flip itself goes out of the box on hover (remove perspective if you don't want the 3D effect */
-.flip-box {
-  width:200px;
-  height: 300px;
-  background-color:white;    
-  perspective: 1000px; /* Remove this if you don't want the 3D effect */
-  border-radius: 20px;
-}
-
-/* This container is needed to position the front and back side */
-.flip-box-inner {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  transition: transform 0.5s;
-  transform-style: preserve-3d;
-  border-radius: 20px;
-}
-
-/* Do an horizontal flip when you move the mouse over the flip box container */
-.flip-box:hover .flip-box-inner {
-  transform: rotateY(180deg);
-  border-radius: 20px;
-}
-
-/* Position the front and back side */
-.flip-box-front, .flip-box-back {
-  position: absolute;
-  width: 100%;
-  height: 100%;  
-  backface-visibility:hidden;
-  border-radius: 20px;
-}
-
-/* Style the front side (fallback if image is missing) */
-.flip-box-front {
-  background-color: red;
-  color: black;
-  border-radius: 20px;
-}
-
-/* Style the back side */
-.flip-box-back {
-  background-color:grey ;
-  color: rgb(5, 5, 5);
-  transform: rotateY(180deg);
-  border-radius: 20px;
-} 
-
-.carousel {
-  height: 100%;
-  width: 100%;
-  background-size: 100% 100%;
-  background-color: #f7f8fa;
-  
-  font-family: "Fredoka One";
-  font-weight: 600;
-  font-size: 30px;
-  color: #3d404f;
-  .carouselslide {
-    padding: 15 px;
-    height: 80%;
-    width: 80%;
-    margin: auto;
-  
-    .card-text {
-      font-size: 10px;
-    }
-  }
-}
-  
 </style>
