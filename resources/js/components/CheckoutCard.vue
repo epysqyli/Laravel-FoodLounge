b5e60a97cccb035b2d518bcedbca3d87a6f840ad
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-10-12 00:09:54 +0200

    stripe

--- resources/js/components/CheckoutCard.vue
+++ resources/js/components/CheckoutCard.vue
@@ -1,15 +1,45 @@
 <template>
   <div>
-
+       <div>
+    <stripe-checkout
+      ref="checkoutRef"
+      mode="payment"
+      :pk="publishableKey"
+      :line-items="lineItems"
+      :success-url="successURL"
+      :cancel-url="cancelURL"
+      @loading="v => loading = v"
+    />
+    <button @click="submit">Pay now!</button>
+  </div>
   </div>
 </template>
 
 <script>
 import { StripeCheckout } from '@vue-stripe/vue-stripe';
 export default {
-    name: 'CheckoutCard',
   components: {
     StripeCheckout,
+  },
+  data () {
+    this.publishableKey = process.env.STRIPE_PUBLISHABLE_KEY;
+    return {
+      loading: false,
+      lineItems: [
+        {
+          price: 6, // The id of the one-time price you created in your Stripe dashboard
+          quantity: 1,
+        },
+      ],
+      successURL: 'your-success-url',
+      cancelURL: 'your-cancel-url',
+    };
+  },
+  methods: {
+    submit () {
+      // You will be redirected to Stripe's secure checkout page
+      this.$refs.checkoutRef.redirectToCheckout();
+    },
   },
 };
 </script>
