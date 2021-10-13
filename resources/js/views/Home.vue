0b5696caa649c6b7028d7d373cd66d3ca74bcbc1
La-wii <sentilavale@gmail.com>
2021-10-13 14:25:51 +0200

    btn hover + bcg-color

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,39 +1,44 @@
 <template>
-  <div class="container mx-auto">
-    <div class="row">
-      <div class="col-12 text-center p-5">
-        <h2 class="text-center mx-auto">
-          Bee Happy, Bee Healthy! Choose: 
-        </h2>
+  <!-- start content -->
+  <div class="content">
+    <div class="container mx-auto">
+      <div class="row">
+        <div class="col-12 text-center p-5">
+          <h2 class="text-center mx-auto">
+            Bee Happy, Bee Healthy! Choose: 
+          </h2>
+        </div>
       </div>
-    </div>
 
-    <div class="row">
-      <div
-        class="col-10 offset-1 offset-sm-0 col-sm-6 col-md-4 col-lg-3 my-2"
-        v-for="category in categories"
-        :key="category.id"
-      >
-        <CategoryCard
-          :category="category"
-          @toggleChoice="toggleChoice(category.slug)"
-        />
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-12">
-        <router-link
-          style="text-decoration: none; color: inherit"
-          :to="{ name: 'categories', params: { names: queryChoices } }"
+      <div class="row">
+        <div
+          class="col-10 offset-1 offset-sm-0 col-sm-6 col-md-4 col-lg-3 my-2"
+          v-for="category in categories"
+          :key="category.id"
         >
-          <div class="d-block w-50 btn btn-outline-primary mx-auto mt-5" v-if="userChoices.length != 0">
-            Go
-          </div>
-        </router-link>
+          <CategoryCard
+            :category="category"
+            @toggleChoice="toggleChoice(category.slug)"
+          />
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-12 mt-4">
+          <router-link
+            style="text-decoration: none; color: inherit"
+            :to="{ name: 'categories', params: { names: queryChoices } }"
+          >
+            <div class="d-block w-50 btn btn-outline-primary mx-auto mt-5 bg-light" v-if="userChoices.length != 0">
+              <!-- Go -->
+              <span>Go</span>
+            </div>
+          </router-link>
+        </div>
       </div>
     </div>
   </div>
+  <!-- end content -->
 </template>
 
 <script>
@@ -94,4 +99,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.content{
+  background-color: #4FB3E4;
+  height: 100%;
+  overflow: hidden;
+  .btn{
+    &:hover{
+      box-shadow: 5px 10px 8px -6px black;
+      background-color: hsl(10%, 10%, 80%);
+      cursor: pointer;
+    }
+    &:hover span{
+      color: #4FB3E4;
+    }
+  }
+}
 </style>    
