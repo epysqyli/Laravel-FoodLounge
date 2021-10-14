e72be5e93f02186a2c41e1585bc1733f23271fb1
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-14 20:38:02 +0200

    Add responsive layout to admin.home

--- resources/js/deleteConfirm.js
+++ resources/js/deleteConfirm.js
@@ -1,7 +1,7 @@
 const deleteBtns = [...document.getElementsByClassName("post-delete")];
 deleteBtns.forEach(btn => {
     btn.addEventListener("click", e => {
-        const userResponse = window.confirm("Are you sure?");
+        const userResponse = window.confirm("Sei sicuro di voler eliminare questo prodotto?");
 
         if (!userResponse) {
             e.preventDefault();
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container backend">
         <div class="row mx-1 pt-4 rounded border bg-light">
-            <div class="col-4">
+            <div class="col-12 col-md-4">
                 <div class="row">
                     <div class="col-8 offset-2">
                         <img class="img-fluid w-100 mt-1 mb-2 rounded shadow-sm"
@@ -11,32 +11,32 @@
                     </div>
                 </div>
                 <div class="row">
-                    <a class="col-7 mx-auto my-2 pt-2 pb-1 border rounded shadow bg-info text-white"
+                    <a class="col-10 col-md-8 mx-auto my-2 pt-2 pb-1 border rounded shadow bg-info text-white"
                         href="{{ route('admin.foods.index', Auth::user()->id) }}">
-                        <h4 class="text-center">Manage Foods</h4>
+                        <h5 class="text-center">Manage Foods</h5>
                     </a>
                 </div>
                 <div class="row">
-                    <a class="col-7 mx-auto my-2 pt-2 pb-1 border rounded shadow bg-info text-white"
+                    <a class="col-10 col-md-8 mx-auto my-2 pt-2 pb-1 border rounded shadow bg-info text-white"
                         href="{{ route('admin.types.index', Auth::user()->id) }}">
-                        <h4 class="text-center">Manage Types</h4>
+                        <h5 class="text-center">Manage Types</h5>
                     </a>
                 </div>
                 <div class="row">
-                    <a class="col-7 mx-auto my-2 pt-2 pb-1 border rounded shadow bg-info text-white"
+                    <a class="col-10 col-md-8 mx-auto my-2 pt-2 pb-1 border rounded shadow bg-info text-white"
                         href="{{ route('admin.orders.index', Auth::user()->id) }}">
-                        <h4 class="text-center">Check Orders</h4>
+                        <h5 class="text-center">Check Orders</h5>
                     </a>
                 </div>
                 <div class="row">
-                    <a class="col-7 mx-auto my-2 pt-2 pb-1 border rounded shadow bg-info text-white"
+                    <a class="col-10 col-md-8 mx-auto my-2 pt-2 pb-1 border rounded shadow bg-info text-white"
                         href="{{ route('admin.users.edit', Auth::user()->id) }}">
-                        <h4 class="text-center">Edit Profile</h4>
+                        <h5 class="text-center">Edit Profile</h5>
                     </a>
                 </div>
             </div>
-            <div class="col-8">
-                <img class="rounded shadow" src={{ asset('storage/' . Auth::user()->cover_image) }} alt="John"
+            <div class="col-12 col-md-8 my-5 my-md-0">
+                <img class="rounded shadow" src={{ asset('storage/' . Auth::user()->cover_image) }} alt="cover image"
                     style="width:100%">
                 <h1 class="title my-3">{{ Auth::user()->name }}</h1>
                 <p class="lead">
