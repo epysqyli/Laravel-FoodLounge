8d9a6dd476ab284eea3cc8fa8f4d08ddef202258
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-14 21:11:55 +0200

    Change button width in foods index

--- resources/views/admin/foods/index.blade.php
+++ resources/views/admin/foods/index.blade.php
@@ -2,25 +2,25 @@
 
 @section('content')
     <div class="container">
-    <p>
-    @if (session('updated'))
-    <div class="alert alert-success">
-      {{session('updated')}}
-    </div>
-      
-    @endif
-    @if (session('delete'))
-    <div class="alert alert-success">
-      {{session('delete')}}
-    </div>
-    @endif
-  </p>
+        <p>
+            @if (session('updated'))
+                <div class="alert alert-success">
+                    {{ session('updated') }}
+                </div>
+
+            @endif
+            @if (session('delete'))
+                <div class="alert alert-success">
+                    {{ session('delete') }}
+                </div>
+            @endif
+        </p>
         <div class="row">
             <div class="col">
                 <h1>All foods</h1>
                 <div class="row">
                     <div class="col-12">
-                        <a class="btn btn-outline-dark mt-2 mb-5 d-block mx-auto w-25"
+                        <a class="btn btn-outline-dark mt-2 mb-5 d-block mx-auto w-75"
                             href="{{ route('admin.foods.create', Auth::user()->id) }}" class="link-dark">ADD FOOD</a>
                     </div>
                 </div>
@@ -46,7 +46,8 @@
                                 <td><a href="{{ route('admin.foods.show', $food->id) }}">Check Details</a></td>
                                 <td><a href="{{ route('admin.foods.edit', $food->id) }}">Change</a></td>
                                 <td>
-                                    <form class="post-delete" action="{{ route('admin.foods.destroy', $food->id) }}" method="post">
+                                    <form class="post-delete" action="{{ route('admin.foods.destroy', $food->id) }}"
+                                        method="post">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" class="btn btn-danger">Delete</button>
