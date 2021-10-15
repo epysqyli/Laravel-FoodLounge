ee38dca5b8c5d416eb3fbfc46cca3040d1c9f6cb
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-15 18:33:20 +0200

    Add reverse animation ot CategoryCard, still to apply

--- resources/js/components/CategoryCard.vue
+++ resources/js/components/CategoryCard.vue
@@ -22,19 +22,20 @@ export default {
   data() {
     return {
       on: "selected",
-      jumpAnimation: 'jump-animation'
+      off: "deselected",
+      jumpAnimation: "jump-animation",
     };
   },
 
   methods: {
     removeJumpAnimation() {
       this.jumpAnimation = null;
-    }
+    },
   },
 
   mounted() {
     setTimeout(this.removeJumpAnimation, 1000);
-  }
+  },
 };
 </script>
 
@@ -71,6 +72,10 @@ export default {
   animation: pressed 100ms ease-in 1 alternate forwards;
 }
 
+.deselected {
+  animation: unpressed 1000ms ease-out 1 alternate forwards;
+}
+
 @keyframes pressed {
   0% {
     box-shadow: 5px 10px 8px -6px black;
@@ -98,8 +103,36 @@ export default {
   }
 }
 
+// still to setup
+@keyframes unpressed {
+  0% {
+    box-shadow: 0px 2px 5px -2px black inset;
+    background-color: hsl(40, 65%, 50%);
+  }
+  25% {
+    box-shadow: 0px 2px 2px 1px black inset;
+    background-color: #e2b33c;
+  }
+  50% {
+    box-shadow: 0px 1px 1px 0px black inset;
+    background-color: #e2b33c;
+  }
+  65% {
+    box-shadow: 0px 0px 0px 0px black;
+    background-color: #e2b33c;
+  }
+  83% {
+    box-shadow: 2px 5px 4px -3px black;
+    background-color: #e2b33c;
+  }
+  100% {
+    background-color: #e9c46a;
+    box-shadow: 3px 6px 8px -6px black;
+  }
+}
+
 .jump-animation {
-  animation: jumpAnimation 1s ease;
+  animation: jumpAnimation 1s ease-in 1 forwards;
 }
 
 @keyframes jumpAnimation {
