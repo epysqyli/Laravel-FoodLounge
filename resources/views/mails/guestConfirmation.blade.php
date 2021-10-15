ac88a4c6469be2add36180b871dc73c831b19466
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-15 13:36:43 +0200

    Style guestConfirmation email

--- resources/views/mails/guestConfirmation.blade.php
+++ resources/views/mails/guestConfirmation.blade.php
@@ -1,3 +1,35 @@
-<h2>
-    This is a mail
-</h2>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Il tuo ordine</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-10 mx-auto">
+                <h1 class="mt-3 text-center">Ordine Confermato!</h1>
+                <h3>Ciao {{ $order->customer_name }}, il tuo ordine sta arrivando!</h3>
+                <h3>L'indirizzo da te indicato: {{ $order->customer_address }}</h3>
+
+
+                <div class="row border rounded d-flex justify-content-around align-items-center py-2 my-2 bg-secondary text-white">
+                    @foreach ($order->foods as $food)
+                        <div class="col-6">{{ $food->name }}</div>
+                        <div class="col-6">{{ $food->pivot->food_units }} unit&agrave;</div>
+                    @endforeach
+                </div>
+                <div class="text-center mt-4 mb-2">Deliveboo &trade;</div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
