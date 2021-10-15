6588862f4f0007a11504f206d3f22d4d11878cdd
Flavio <flavio.origgi@outlook.it>
2021-10-15 12:23:03 +0200

    cambiato layout adminHome e linguaggio di altre blade in admin

--- resources/views/admin/foods/index.blade.php
+++ resources/views/admin/foods/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
     <div class="container">
@@ -7,7 +7,6 @@
                 <div class="alert alert-success">
                     {{ session('updated') }}
                 </div>
-
             @endif
             @if (session('delete'))
                 <div class="alert alert-success">
@@ -21,7 +20,7 @@
                 <div class="row">
                     <div class="col-12">
                         <a class="btn btn-outline-dark mt-2 mb-5 d-block mx-auto w-75"
-                            href="{{ route('admin.foods.create', Auth::user()->id) }}" class="link-dark">ADD FOOD</a>
+                            href="{{ route('admin.foods.create', Auth::user()->id) }}" class="link-dark text-uppercase">aggiungi prodotto</a>
                     </div>
                 </div>
                 <div class="row ">
@@ -32,14 +31,14 @@
                                 <td>{{ $food->name }}</td>
                                 <td>{{ $food->price }} &euro;</td>
                                 <td>{{ $food->visible ? 'Visible' : 'Not Visible' }}</td>
-                                <td><a href="{{ route('admin.foods.show', $food->id) }}">Check Details</a></td>
-                                <td><a href="{{ route('admin.foods.edit', $food->id) }}">Change</a></td>
+                                <td><a href="{{ route('admin.foods.show', $food->id) }}">Dettagli</a></td>
+                                <td><a href="{{ route('admin.foods.edit', $food->id) }}">Modifica</a></td>
                                 <td>
                                     <form class="post-delete" action="{{ route('admin.foods.destroy', $food->id) }}"
                                         method="post">
                                         @csrf
                                         @method('DELETE')
-                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                        <button type="submit" class="btn btn-danger">Elimina</button>
                                     </form>
                                 </td>
                             </tr>
@@ -52,8 +51,8 @@
                                     <td>{{ $food->name }}</td>
                                     <td>{{ $food->price }} &euro;</td>
                                     <td>{{ $food->visible ? 'Sì' : 'No' }}</td>
-                                    <td><a href="{{ route('admin.foods.show', $food->id) }}">Visiona Dettagli</a></td>
-                                    <td><a href="{{ route('admin.foods.edit', $food->id) }}">Modifica</a></td>
+                                    <td><a href="{{ route('admin.foods.show', $food->id) }}">Visiona</a></td>
+                                    <td><a href="{{ route('admin.foods.edit', $food->id) }}">Esegui</a></td>
                                     <td>
                                         <form class="post-delete" action="{{ route('admin.foods.destroy', $food->id) }}" method="post">
                                             @csrf
@@ -72,5 +71,70 @@
         <a href="{{ Route('admin.home') }}" class="btn btn-secondary text-white">
             <span>Torna indietro</span>
         </a>
+    </div>
+@endsection --}}
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <p>
+            @if (session('updated'))
+                <div class="alert alert-success">
+                    {{session('updated')}}
+                </div>            
+            @endif
+            @if (session('delete'))
+                <div class="alert alert-success">
+                    {{session('delete')}}
+                </div>
+            @endif
+        </p>
+        <div class="row mb-2">
+            <div class="col">
+                <h1>Tutti i prodotti</h1>
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <a class="btn btn-outline-dark mt-2 mb-5 text-uppercase"
+                            href="{{ route('admin.foods.create', Auth::user()->id) }}" class=" link-dark">aggiungi prodotto</a>
+                    </div>
+                </div>
+                <table class="table table-light table-striped">
+                    <thead>
+                        <tr>
+                            <th>Id Prodotto</th>
+                            <th>Nome</th>
+                            <th>Prezzo</th>
+                            <th>Disponibilà</th>
+                            <th>Dettagli</th>                            
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        @foreach ($foods as $food)
+                            <tr>
+                                <td>{{ $food->id }}</td>
+                                <td>{{ $food->name }}</td>
+                                <td>{{ $food->price }} &euro;</td>
+                                <td>{{ $food->visible ? 'Sì' : 'No' }}</td>
+                                <td><a href="{{ route('admin.foods.show', $food->id) }}">Visualizza</a></td>
+                                <td><a href="{{ route('admin.foods.edit', $food->id) }}">Modifica</a></td>
+                                <td>
+                                    <form class="post-delete" action="{{ route('admin.foods.destroy', $food->id) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Elimina</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <a href="{{ Route('admin.home') }}" class="btn btn-secondary text-white">
+            <span>Torna indietro</span>
+        </a> 
     </div>
 @endsection
--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 
@@ -60,5 +60,58 @@
         <a href="{{ Route('admin.home') }}" class="btn btn-secondary text-white mt-2">
             <span>Torna indietro</span>
         </a>
+    </div>
+@endsection --}}
+@extends('layouts.app')
+
+@section('content')
+
+    @if (count(Auth::user()->types) < 3)
+        <div class="row">
+            <div class="col-12">
+                <a class="btn btn-outline-dark mt-2 mb-4 d-block mx-auto w-25" href="{{ route('admin.types.create') }}"
+                    class="link-dark">Add Types</a>
+            </div>
+        </div>
+    @endif
+
+    <div class="container">
+        <div id="accordion">
+            @foreach ($types as $type)
+
+                <div class="card my-3">
+                    <div class="card-header" id="{{ $type->name . 'heading' }}">
+                        <div class="mb-0 d-flex justify-content-between">
+                            <div class="py-2" data-target="#{{ $type->name . 'collapse' }}" aria-expanded="true"
+                                aria-controls="{{ $type->name . 'collapse' }}">
+                                {{ $type->name }}
+                            </div>
+                            <div class="col-4 d-flex justify-content-around">
+                                <a class="btn btn-outline-dark d-block w-50"
+                                    href="{{ route('admin.types.edit', $type->id) }}" class="link-dark">Edit Type</a>
+                                <form method="post" class="post-delete" action={{ route('admin.types.destroy', $type->id) }}>
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="{{ $type->name . 'collapse' }}" class="collapse show"
+                        aria-labelledby="{{ $type->name . 'heading' }}" data-parent="#accordion">
+                        <div class="card-body">
+                            <ul class="list-group">
+                                @foreach ($type->foods as $food)
+                                    <li class="list-group-item"><a
+                                            href={{ route('admin.foods.show', $food->id) }}>{{ $food->name }}</a></li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            @endforeach
+        </div>
     </div>
 @endsection
