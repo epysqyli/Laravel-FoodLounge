839a48f7ede371a87f30782039d65fbdc53d5542
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-15 14:43:58 +0200

    JS delete confirm to ITA

--- resources/js/deleteConfirm.js
+++ resources/js/deleteConfirm.js
@@ -1,7 +1,7 @@
 const deleteBtns = [...document.getElementsByClassName("post-delete")];
 deleteBtns.forEach(btn => {
     btn.addEventListener("click", e => {
-        const userResponse = window.confirm("Are you sure?");
+        const userResponse = window.confirm("Confermi?");
 
         if (!userResponse) {
             e.preventDefault();
