61cb443878287034f083bda6903443094840bd56
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-16 15:47:25 +0200

    Add basic PaymentResult page

--- resources/js/views/PaymentResult.vue
+++ resources/js/views/PaymentResult.vue
@@ -3,14 +3,50 @@
     <h1 class="text-center my-5 py-2">Ordine confermato!</h1>
     <div class="row">
       <div class="col-10 offset-1">
-        <div class="text-center h3 mx-auto">Riepilogo ordine da inserire</div>
+        <div class="text-center h3 mx-auto">
+          {{ orderDetails.customer_name }}, ecco il riepilogo del tuo ordine.
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-10 offset-1 col-md-4 offset-md-4 mx-auto mt-5">
+        <div class="table">
+          <tr>
+            <th>Totale Ordine</th>
+            <td>{{ orderDetails.amount }} &euro;</td>
+          </tr>
+          <tr>
+            <th>Indirizzo di Spedizione</th>
+            <td>{{ orderDetails.customer_address }}</td>
+          </tr>
+          <tr>
+            <th>Prodotti ordinati</th>
+            <td>
+              <ul>
+                <li v-for="item in orderedItems" :key="item.id">
+                  {{ item.name }}
+                </li>
+              </ul>
+            </td>
+          </tr>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-10 offset-1 mt-3">
+        <p class="text-center">
+          Controlla '{{ orderDetails.customer_email }}' per ulteriori
+          informazioni
+        </p>
       </div>
     </div>
     <router-link :to="{ name: 'home' }"
       ><div
         class="text-center btn btn-outline-primary d-block w-75 mx-auto my-5"
       >
-        Go buy more stuff
+        Esplora altri ristoranti!
       </div></router-link
     >
   </div>
