439be6eb59184518e4e31d0452f373026ce2b458
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-18 21:47:51 +0200

    Remove useless style from old Restaurant card

--- resources/js/views/Restaurant.vue
+++ resources/js/views/Restaurant.vue
@@ -1,61 +1,8 @@
 <template>
-  <div class="container-fluid">
-    <div class="row cover_image">
-      <div class="cover-image">
-        <img
-          :src="restaurant.cover_image"
-          :alt="restaurant.name"
-          class="image"
-        />
-
-        <div class="middle">
-          <div class="text text-uppercase">{{ restaurant.name }}</div>
-        </div>
-        <div class="address">
-          <div class="text-uppercase">{{ restaurant.address }}</div>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-8">
-        <div class="row">
-          <div class="col p-5" v-for="(food, index) in foods" :key="index">
-            <div class="card">
-              <div class="card-body">
-                <!-- <img
-                  class="rounded"
-                  :src="
-                    food.image[0] == 'h'
-                      ? food.image
-                      : `http://localhost:8000/storage/${food.image}`
-                  "
-                  :alt="food.name"
-                /> -->
-                <div class="card-date">
-                  <i class="bi bi-calendar3"></i>24 Mar 18:00 PM
-                </div>
-                <p class="card-text">
-                  {{ food.name }}
-                </p>
-                <div><i class="bi bi-person"></i>Teacher:</div>
-                <br />
-                <div><i class="bi bi-cash"></i>Price: {{ food.price }}</div>
-                <a href="#" class="btn btn-primary"
-                  ><i class="bi bi-plus-lg"></i>Find More</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-4 bg-secondary">2 di 2</div>
-    </div>
-
-    <!-- Display foods -->
+  <div class="container">
     <table class="table">
       <thead>
         <tr>
-          <th scope="col">#</th>
           <th scope="col">Image</th>
           <th scope="col">Price</th>
           <th scope="col">Name</th>
@@ -64,10 +11,9 @@
       </thead>
       <tbody>
         <tr v-for="(food, index) in foods" :key="index">
-          <th scope="row">1</th>
           <td>
             <img
-              class=""
+              class="w-25"
               :src="
                 food.image[0] == 'h'
                   ? food.image
@@ -278,117 +224,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.card {
-  margin: 20px;
-  border-radius: 25px;
-  min-width: 270px;
-  max-height: 300px;
-  box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
-  .card-date {
-    margin-top: 20px;
-  }
-  p {
-  }
-  .btn {
-    margin-top: 20px;
-    height: 50px;
-    border-radius: 25px;
-    vertical-align: middle;
-    padding: 16px 43px !important;
-    background-color: #e56768;
-    box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
-    .card-date {
-      margin-top: 20px;
-    }
-    .btn {
-      margin-top: 20px;
-      height: 50px;
-      border-radius: 25px;
-      vertical-align: middle;
-      padding: 16px 43px !important;
-      background-color: #e56768;
-      box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
-      border: 0px;
-      &:hover {
-        background-color: #e9d758;
-      }
-    }
-    img {
-      height: 80px;
-      width: 100px;
-      transition: transform 0.8s;
-      &:hover {
-        transform: scale(1.1);
-      }
-    }
-  }
-}
-
-.container-fluid {
-  .cover_image:hover .image {
-    opacity: 0.8;
-  }
-  .cover_image:hover .middle,
-  .cover_image:hover .address {
-    opacity: 1;
-  }
-  .cover-image {
-    position: relative;
-    width: 100%;
-
-    .text {
-      background: rgb(0, 0, 0); /* Fallback color */
-      background: rgba(0, 0, 0, 0.9); /* Black background with 0.5 opacity */
-      color: #eb7b0c; /* Grey text */
-      font-size: 16px;
-      padding: 16px 32px;
-      animation: type 8s steps(60, end);
-    }
-
-    img {
-      width: 100%;
-      height: 20px;
-      object-fit: cover;
-      opacity: 1;
-      transition: 0.5s ease;
-      backface-visibility: hidden;
-    }
-    .middle {
-      transition: 0.5s ease;
-      opacity: 0;
-      position: absolute;
-      top: 20%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      -ms-transform: translate(-50%, -50%);
-      text-align: center;
-    }
-    .address {
-      transition: 0.5s ease;
-      opacity: 0;
-      position: absolute;
-      text-align: center;
-      bottom: 5px;
-      left: 10px;
-      color: black;
-      margin-left: 40px;
-    }
-  }
-}
-td img {
-  height: 10px;
-  width: 50px;
-  object-fit: cover;
-  vertical-align: middle;
-}
-h2 {
-  font-size: 40px;
-  color: #ffd60a;
-}
-.card {
-  border: none;
-}
-
 .notClickable {
   opacity: 0.25;
   pointer-events: none;
