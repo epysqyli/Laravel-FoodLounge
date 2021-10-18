c142720dbd12955e1189ed5068edf7c5ef4533eb
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-10-18 15:47:50 +0200

    Add font-awesome

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,11 +1,19 @@
 require("./bootstrap");
 
-import Vue from 'vue';
+import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faShoppingCart);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 const app = new Vue({
     el: "#root",
-    render: h => h(App),
-    router
+    render: (h) => h(App),
+    router,
 });
